3055a5208e9fd92c4a523f4f22d5d0a54397e0bd
Eyüp Birankar <eyup.birankar@outlook.com>
2021-12-27 01:30:12 +0300

    sql migration

--- IdentityServer/Interstellar.FreeCourse.IdentityServer/Program.cs
+++ IdentityServer/Interstellar.FreeCourse.IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using Interstellar.FreeCourse.IdentityServer.Data;
+using Interstellar.FreeCourse.IdentityServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,23 +41,37 @@ namespace Interstellar.FreeCourse.IdentityServer
 
             try
             {
-                var seed = args.Contains("/seed");
-                if (seed)
-                {
-                    args = args.Except(new[] { "/seed" }).ToArray();
-                }
+                //Migrationların otomatik olmasını istioruz.
+                //Uygulama ayağa kalktığı zaman bir veri tabanı oluşsun,+++
+                //+++migration varsa bu migrationlar gerçekleşsin uygulama öyle ayağa kalksın istioruz.
 
                 var host = CreateHostBuilder(args).Build();
 
-                if (seed)
+                using (var scope = host.Services.CreateScope())
                 {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
+                    var serviceProvider = scope.ServiceProvider;
+
+                    var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>(); //GetRequiredService --> Service'yi bulamazsa Exception döner "GetService"--> service'yi bulamazsa null döner.
+
+                    applicationDbContext.Database.Migrate(); //veri tabanı yoksa oluşturacak, uygulamamış migration yoksa onlarıda uygulayacak.
+
+                    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    if (!userManager.Users.Any())
+                    {
+                        userManager.CreateAsync(new ApplicationUser { UserName = "BirankarE", Email = "eyup.birankar@outlook.com", City = "Ankara" }, "sifre123456").Wait();
+                    }
+                    //if (!userManager.Users.Any())
+                    //{
+                    //    userManager.CreateAsync(new ApplicationUser
+                    //    {
+                    //        UserName = "BirankarE",
+                    //        Email = "eyup.birankar@outlook.com",
+                    //        City = "İstanbul"
+                    //    }, "sifre123456").Wait();
+                    //}
                 }
+
 
                 Log.Information("Starting host...");
                 host.Run();
